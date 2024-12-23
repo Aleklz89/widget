eed35de97a348b19e5e44a27561d50f9d34c31bb
Aleklz90 <novogogosha@gmail.com>
2024-12-23 23:49:34 +0200

    global settings

--- widget.js
+++ widget.js
@@ -101,7 +101,7 @@ class ProductSearchWidget {
         }
 
         // 1) Загружаем HTML
-          const resp = await fetch('https://aleklz89.github.io/widget/widget.html');
+        const resp = await fetch('https://aleklz89.github.io/widget/widget.html');
         // const resp = await fetch('widget.html');
         const widgetHtml = await resp.text();
 
@@ -770,8 +770,8 @@ class ProductSearchWidget {
         resultContainer.innerHTML = '';
 
         // Грузим шаблон
-        // const tResp = await fetch('https://aleklz89.github.io/widget/product-item.html');
-        const tResp = await fetch('product-item.html');
+        const tResp = await fetch('https://aleklz89.github.io/widget/product-item.html');
+        // const tResp = await fetch('product-item.html');
         if (!tResp.ok) throw new Error(`Failed to load product template: ${tResp.status}`);
         const productTemplate = await tResp.text();
 
